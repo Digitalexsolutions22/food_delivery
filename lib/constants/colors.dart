43991115048bf88ff5fb93e15f700c9d783e3f87
Janakiraman I <digitalexsolutions@Janakiramans-Mac-mini.local>
2025-07-22 15:05:22 +0530

    Initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color white = Color(0xFFFFFFFF);       // #FFFFFF
+  static const Color black = Color(0xFF000000);       // #000000
+  static const Color orange = Color(0xFFFF6B00);      // #FF6B00
+  static const Color lightOrange = Color(0xFFFD8E3D); // #FD8E3D
+  static const Color gold = Color(0xFFFFD700);        // #FFD700
+  static const Color grayDark = Color(0xFF676666);    // #676666
+  static const Color blue = Color(0xFF0040FF);        // #0040FF
+  static const Color gray = Color(0xFF939393);        // #939393
+  static const Color grayLight = Color(0xFFEBEBEB);   // #EBEBEB
+  static const Color purple = Color(0xFFA632FF);      // #A632FF
+  static const Color green = Color(0xFF00C117);       // #00C117
+  static const Color blueLight = Color(0xFF5DB1F0);   // #5DB1F0
+  static const Color blueTransparent = Color(0x245DB1F0); // #5DB1F024
+  static const Color grayMedium = Color(0xFF9B9B9B);  // #9B9B9B
+  static const Color purpleTransparent = Color(0x24B38DD0); // #B38DD024
+  static const Color blueExtraLight = Color(0xFFEFF6FF); // #EFF6FF
+  static const Color greenExtraLight = Color(0xFFF0FDF4); // #F0FDF4
+  static const Color purpleExtraLight = Color(0xFFFAF5FF); // #FAF5FF
+  static const Color yellow = Color(0xFFFFBB18);      // #FFBB18
+  static const Color yellowTransparent = Color(0x24FFD369); // #FFD36924
+  static const Color cream = Color(0xFFFFF7ED);       // #FFF7ED
+}
